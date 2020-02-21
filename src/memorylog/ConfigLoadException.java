133ed082b3274cd5a1e676693f849dcf06f10da8
Dylan <dylanlsutton@gmail.com>
2020-02-21 01:45:23 -0500

    Add a custom exception class for Config file load failures.

---
+++ src/memorylog/ConfigLoadException.java
@@ -0,0 +1,9 @@
+package memorylog;
+
+public class ConfigLoadException extends Exception {
+	public ConfigLoadException() {}
+
+	public ConfigLoadException(String message) {
+		super(message);
+	}
+}
