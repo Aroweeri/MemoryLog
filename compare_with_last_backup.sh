b7c681ab4f417a0a3dca30882a437f926cfc883d
Dylan <dylanlsutton@gmail.com>
2020-02-24 02:00:13 -0500

    compare_with_last_backup.sh to use the value in config.txt, add run.sh for running SubjectTester with extra output

--- compare_with_last_backup.sh
+++ compare_with_last_backup.sh
@@ -1,2 +1,4 @@
-colordiff backups/$(ls -1 backups | tail -n 1) src/memorylog/auto_memory_log.txt
-wc -l src/memorylog/auto_memory_log.txt backups/$(ls -1 backups | tail -n 1)
+memlogPath=$(grep "MEMLOGPATH" config.txt | cut -d= -f2)
+
+colordiff backups/$(ls -1 backups | tail -n 1) "$memlogPath" 
+wc -l "$memlogPath"  backups/$(ls -1 backups | tail -n 1)
