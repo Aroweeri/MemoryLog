642de51166164bf9712a476e17c875c069da0eb7
Dylan Sutton <dylanlsutton@gmail.com>
2020-11-03 19:04:21 -0500

    Complete test for MemoryLog class.

--- src/tests/MemoryLogTest.java
+++ src/tests/MemoryLogTest.java
@@ -7,15 +7,15 @@ import static org.junit.Assert.*;
 public class MemoryLogTest {
 
 	@Test
-	public void shouldSetDay() {
+	public void shouldOpenDefaultConfigAndXML() {
 			try {
-				MemoryLog m = new MemoryLog("testconfig1.txt");
+				MemoryLog m = new MemoryLog();
 			} catch (java.io.FileNotFoundException e) {
 				fail("FileNotFoundException abnormal case");
 			} catch (ConfigLoadException e) {
 				fail("ConfigLoadException abnormal case");
 			} catch (javax.xml.bind.JAXBException e) {
-				fail("JAXBException e abornal case");
+				fail("JAXBException abnormal case");
 			}
 	}
 }
